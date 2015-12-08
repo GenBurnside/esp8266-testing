d9b173c39254ebb596eab377c984b3a08fd732ce
matt <matthughes.tech@gmail.com>
2015-12-08 00:04:38 -0500

    Connect to NIST to get UTC time

--- esp8266_testing.ino
+++ esp8266_testing.ino
@@ -2,17 +2,15 @@
  
 const char* ssid     = "yourssid";
 const char* password = "yourpassword";
- 
-const char* host = "www.adafruit.com";
- 
+
+const char* timeHost = "time.nist.gov";
+const char* dataHost = "data.sparkfun.com";
+
 void setup() {
   Serial.begin(115200);
   delay(100);
  
   // We start by connecting to a WiFi network
- 
-  Serial.println();
-  Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
   
@@ -27,42 +25,45 @@ void setup() {
   Serial.println("WiFi connected");  
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+  Serial.println("");
 }
  
-int value = 0;
- 
 void loop() {
-  delay(5000);
-  ++value;
- 
   Serial.print("connecting to ");
-  Serial.println(host);
+  Serial.println(timeHost);
   
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
+  const int timePort = 13;
+  const int dataPort = 80;
+
+  if (!client.connect(timeHost, timePort)) {
     Serial.println("connection failed");
     return;
-  }
+  }  
   
-  // We now create a URI for the request
-  String url = "/testwifi/index.html";
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
-  
-  // This will send the request to the server
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
-               "Connection: close\r\n\r\n");
-  delay(500);
-  
+  // Send the request to the server
+  client.print("HEAD / HTTP/1.1\r\nAccept: */*\r\nUser-Agent: Mozilla/4.0 (compatible; ESP8266 NodeMCU Lua;)\r\n\r\n");
+  delay(100);
+
   // Read all the lines of the reply from server and print them to Serial
+  // expected line is like : Date: Thu, 01 Jan 2015 22:00:14 GMT
   while(client.available()){
     String line = client.readStringUntil('\r');
-    Serial.print(line);
+    if (line.indexOf("UTC(NIST)") == -1)
+    {
+      Serial.println("Unexpected response from NIST");
+    }
+    else
+    {
+      // date starts at pos 7
+      String dateTime = line.substring(7, 24) + " UTC";
+      Serial.println(dateTime);
+    }
   }
   
   Serial.println();
   Serial.println("closing connection");
+
+  delay(60000);
 }
